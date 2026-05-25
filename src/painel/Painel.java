69182d62e943014b2c2ff9880ff783d9429ef1ea
Pedro Barreto <pedro1barreto96@gmail.com>
2026-05-25 17:00:41 -0300

    feat: colisão de objetos

--- src/painel/Painel.java
+++ src/painel/Painel.java
@@ -4,12 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyListener;
 import java.awt.event.*;
+import java.util.Random;
 
 public class Painel extends JPanel implements KeyListener {
     public Painel(){
         setBackground(Color.black);
         Timer timer = new Timer(100, e -> {
             MoveSnake();
+            Colision();
             repaint();
         });
         timer.start();
@@ -17,36 +19,54 @@ public class Painel extends JPanel implements KeyListener {
         addKeyListener(this);
     }
 
+    private Random random = new Random();
 
     private int SnakeX = 240;
     private int SnakeY = 250;
 
     private String direction = "RIGHT";
 
+    private int AppleX = 100;
+    private int Appley = 30;
 
     public void MoveSnake() {
-            if(direction.equals("RIGHT")) {
-                SnakeX += 10;
-            }
-            if (direction.equals("LEFT")) {
-                SnakeX -= 10;
-            }
-            if(direction.equals("UP")) {
-                SnakeY -= 10;
-            }
-            if(direction.equals("DOWN")){
-                SnakeY += 10;
-            }
+        if(direction.equals("RIGHT")) {
+            SnakeX += 10;
+        }
+        if (direction.equals("LEFT")) {
+            SnakeX -= 10;
+        }
+        if(direction.equals("UP")) {
+            SnakeY -= 10;
+        }
+        if(direction.equals("DOWN")){
+            SnakeY += 10;
+        }
+
+        if (SnakeX >= 1000){
+                SnakeX = 0;
+        } else if (SnakeX <= 0) {
+                SnakeX = 990;
+        } else if (SnakeY >= 1000) {
+                SnakeY = 0;
+        } else if (SnakeY <= 0) {
+            SnakeY = 990;
+        }
     }
 
+    public void Colision(){
+        if (SnakeX == AppleX &&  SnakeY == Appley){
+            AppleX =  random.nextInt(100) * 10;
+            Appley =  random.nextInt(100) * 10;
+        }
+    }
 
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
         g.setColor(Color.green);
-        int AppleX = 100;
-        int Appley = 30;
+
         int AppleSizeX = 20;
         int AppleSizeY = 20;
         g.fillOval(AppleX, Appley, AppleSizeX, AppleSizeY);
